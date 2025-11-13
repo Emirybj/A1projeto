233becd490555df1e4f437cead234b30ae2d07e6
Emirybj <emilyperfect76@gmail.com>
2025-11-13 14:59:37 -0300

    adicionando comentarios

--- app/src/main/java/com/example/a1projeto/ui/AppNavigation.kt
+++ app/src/main/java/com/example/a1projeto/ui/AppNavigation.kt
@@ -1,6 +1,6 @@
 package com.example.a1projeto.ui
 
-// --- VERIFIQUE SE TODAS ESTAS IMPORTAÇÕES ESTÃO AQUI ---
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,9 +9,7 @@ import com.example.a1projeto.ui.screens.PlaylistScreen
 import com.example.a1projeto.ui.screens.PlaylistDetailsScreen
 import com.example.a1projeto.ui.screens.SongSearchScreen
 
-// --------------------------------------------------
 
-// Um objeto 'sealed' é ótimo para definir rotas de forma segura
 sealed class Screen(val route: String) {
     object PlaylistList : Screen("playlist_list_screen") // Tela 1
     object PlaylistDetails : Screen("playlist_details_screen") // Tela 2
@@ -26,17 +24,17 @@ fun AppNavigation() {
         navController = navController,
         startDestination = Screen.PlaylistList.route // Define a tela inicial
     ) {
-        // Rota para a Tela 1: Lista de Playlists
+        //  Lista de Playlists
         composable(route = Screen.PlaylistList.route) {
             PlaylistScreen(navController = navController)
         }
 
-        // Rota para a Tela 2: Detalhes da Playlist
+        // Detalhes da Playlist
         composable(route = Screen.PlaylistDetails.route) {
             PlaylistDetailsScreen(navController = navController)
         }
 
-        // Rota para a Tela 3: Busca de Músicas
+        // Busca de Músicas
         composable(route = Screen.SongSearch.route) {
             SongSearchScreen(navController = navController)
         }
